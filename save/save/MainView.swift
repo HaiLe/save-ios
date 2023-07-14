6836b2fd1f6a50fa82cf20d2ea81719ef5d64cc6
Hai Le <honghaile@gmail.com>
2023-07-14 07:01:18 -0700

    update the add an expense view

--- save/save/MainView.swift
+++ save/save/MainView.swift
@@ -50,11 +50,43 @@ struct AddBudgetView: View {
     }
 }
 
-
 struct AddItemView: View {
+    @State private var dollarAmount = ""
+    @State private var itemName = ""
+    @State private var selectedCategory = "Category 1"
+    
+    // Your categories
+    private let categories = ["Category 1", "Category 2", "Category 3"]
+    
     var body: some View {
-        Text("This is where you can add item related functionality")
-            .padding()
+        Form {
+            TextField("Dollar Amount", text: $dollarAmount)
+                .keyboardType(.decimalPad)
+            
+            TextField("Item Name", text: $itemName)
+            
+            Picker("Category", selection: $selectedCategory) {
+                ForEach(categories, id: \.self) {
+                    Text($0)
+                }
+            }
+            Button(action: {
+                // Handle save action
+                print("Save button tapped!")
+                print("Dollar Amount: \(dollarAmount)")
+                print("Item Name: \(itemName)")
+                print("Selected Category: \(selectedCategory)")
+            }) {
+                Text("Save")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding(.top)
+        }
+        .navigationBarTitle("Add an Item", displayMode: .inline)
     }
 }
 
